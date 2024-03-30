e562bebe2dbbcf3107b8c2042a857b3c0a82ae31
Mateus Correia <correia.mateus@hotmail.com>
2024-03-30 17:47:57 -0300

    Add edit genre

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -9,7 +9,18 @@ class GenresController < ApplicationController
   def create
     @genre = Genre.new(params.require(:genre).permit(:name))
     @genre.save
-    
-    redirect_to root_path
+
+    redirect_to genres_path
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update(params.require(:genre).permit(:name))
+
+    redirect_to genres_path
   end
 end
